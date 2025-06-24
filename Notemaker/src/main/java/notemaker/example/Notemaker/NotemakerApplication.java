947b3f929b3715ef0c138e25fd2cc677bb5d20ab
yash-sharma-robomq <yash.sharma@robomq.io>
2025-06-24 14:20:37 +0530

    Note Maker Application Backend

---
+++ Notemaker/src/main/java/notemaker/example/Notemaker/NotemakerApplication.java
@@ -0,0 +1,13 @@
+package notemaker.example.Notemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemakerApplication.class, args);
+	}
+
+}
---
+++ Notemaker/src/test/java/notemaker/example/Notemaker/NotemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package notemaker.example.Notemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
